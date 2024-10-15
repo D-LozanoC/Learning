263dbe4a45ca614c9912e835fd0f5489b88e40eb
David Lozano <davidalexisloz1@gmail.com>
2024-10-14 22:08:47 -0500

    Create a storage function and refactor some code

--- React/Class 2/tic-tac-toe/src/App.jsx
+++ React/Class 2/tic-tac-toe/src/App.jsx
@@ -7,11 +7,18 @@ import Board from "./components/Board.jsx"
 
 import { TURNS } from "./constants.js"
 import { checkWinnerFrom, checkEndGame } from "./logic/board.js"
+import { saveGameToStorage, resetGameStorage } from "./logic/storage/index.js"
 
 function App() {
   // Creacion de estados
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(() => {
+    const savedBoard = window.localStorage.getItem('board')
+    return savedBoard ? JSON.parse(savedBoard) : Array(9).fill(null)
+  })
+  const [turn, setTurn] = useState(()=>{
+    const savedTurn = window.localStorage.getItem('turn')
+    return savedTurn ?? TURNS.X
+  })
   const [winner, setWinner] = useState(null)
 
   // Resetear es simplemente setear nuevamente los estados con los que iniciaba
@@ -19,12 +26,14 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    resetGameStorage()
   }
 
   // Actualización del board al hacer click en un cuadrado
   const updateBoard = (index) => {
     // Si ya esta lleno no actualiza la posicion del board
-    if (board[index] || winner){ return;}
+    if (board[index] || winner) { return; }
 
     // Actualización del board
     const newBoard = [...board]; // Los estados siempre se deben tratar como inmutables
@@ -36,13 +45,16 @@ function App() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn)
 
+    // Guardar partida
+    saveGameToStorage({board: newBoard, turn: newTurn})
+
     // Checa si hay un ganador en el nuevo board
     const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
       confetti()
       setWinner(newWinner) // El seteo no es SINCRONO
-    }else if (checkEndGame(newBoard)){
-      setWinner(false) 
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false)
     }
   }
 
@@ -52,13 +64,13 @@ function App() {
     <main className="board">
       <h1>Tic tac toe</h1>
       <button onClick={resetGame}>Reset del juego</button>
-      <Board board={board} updateBoard={updateBoard}/>
+      <Board board={board} updateBoard={updateBoard} />
       <section className="turn">
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
       <section>
-        <WinnerModal resetGame={resetGame} winner={winner}/>
+        <WinnerModal resetGame={resetGame} winner={winner} />
       </section>
     </main>
   )
